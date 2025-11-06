9fe039a092fab83883260f661aa66821e9936a02
Saeed Mahud Rahat <118961502+rahatdevops@users.noreply.github.com>
2025-11-06 20:44:03 +0600

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp /var/www/.env /var/www/laravelblogapp/
-sudo chown -R deployer:deployer /home/deployer/sites/laravelblogapp
+
+sudo chown -R deployer:www-data /var/www/laravelblogapp
+sudo chmod -R 775 /var/www/laravelblogapp/storage /var/www/laravelblogapp/bootstrap/cache
 
 sudo -u deployer bash <<EOF
 cd /var/www/laravelblogapp
