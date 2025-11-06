3365b9ab4a4cdd7bbe14697aaf9df9d20fd3dd6b
Saeed Mahud Rahat <118961502+rahatdevops@users.noreply.github.com>
2025-11-06 15:30:31 +0600

    Update restart_services.sh

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -3,7 +3,7 @@ sudo systemctl restart nginx
 sudo systemctl restart php-fpm8.2
 
 # Switch to deployer user to run Laravel optimizations
-sudo -u deployer bash <<EOF
+#sudo -u deployer bash <<EOF
 cd /var/www/laravelblogapp/
 
 # Run Laravel optimizations
