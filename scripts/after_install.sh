38a2c61b2e36615e6d82d98beb85337ba686707f
Saeed Mahud Rahat <118961502+rahatdevops@users.noreply.github.com>
2025-11-06 16:57:58 +0600

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+cp /var/www/.env /var/www/laravelblogapp/
+sudo chown -R deployer:deployer /home/deployer/sites/laravelblogapp
 
+sudo -u deployer bash <<EOF
+cd /var/www/laravelblogapp
+
+# Install/update dependencies
+composer install --no-dev --prefer-dist --optimize-autoloader
+
+# Set correct permissions for storage and cache
+chmod -R 775 storage bootstrap/cache
+EOF
